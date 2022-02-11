00a7fa036870485a2d1dad7b12f189364707cf01
Gabriel Miranda <70436348+GabriellMiranda@users.noreply.github.com>
2022-02-11 11:19:30 -0300

    Delete Trabalho 3 - Herança directory

--- Trabalho 3 - Herança/Exercicio 3/src/Quadrado.java
+++
@@ -1,5 +0,0 @@
-public class Quadrado extends Retangulo{
-    public Quadrado(float lado, String tipo){
-        super(lado, lado, tipo);
-    }
-}
--- Trabalho 3 - Herança/Exercicio 5/src/Objeto.java
+++
@@ -1,7 +0,0 @@
-
-
-public interface Objeto {
-    public String toString();
-    public String tipoVitoria();
-    public boolean vitoria(Objeto rival);
-}
