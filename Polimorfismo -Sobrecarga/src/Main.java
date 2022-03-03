30122f610646a555ed9d8bcdb0737cd51beccc60
Gabriel Miranda <70436348+GabriellMiranda@users.noreply.github.com>
2022-03-03 11:18:33 -0300

    Sobrecarga

---
+++ Polimorfismo -Sobrecarga/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        Cachorro c = new Cachorro();
+        c.reagir(true);
+        c.reagir("Ola");
+        c.reagir(16,21);
+        c.reagir(5,2);
+
+    }
+}
